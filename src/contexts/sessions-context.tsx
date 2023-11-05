805d2eeff6c4081d0191d97d9b091a4523b83186
Iris Huang <lyaui@outloook.com>
2023-11-05 22:57:09 +0800

    feat(sessions-context): add reducer

--- src/contexts/sessions-context.tsx
+++ src/contexts/sessions-context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, type ReactNode } from 'react';
+import { createContext, useContext, useReducer, type ReactNode } from 'react';
 import { type Session } from '@/components/SessionItem';
 
 export interface BookedSession extends Session {
@@ -9,12 +9,51 @@ export interface BookedSession extends Session {
 interface SessionsContextValues {
   bookedSessions: BookedSession[];
   addSession: (val: BookedSession) => void;
-  deleteSession: () => void;
+  deleteSession: (id: string) => void;
 }
 
 interface SessionsProviderProps {
   children: ReactNode;
 }
+
+interface AddSessionAction {
+  type: 'ADD_SESSION';
+  payload: BookedSession;
+}
+
+interface deleteSessionAction {
+  type: 'DELETE_SESSION';
+  id: string;
+}
+
+type Action = AddSessionAction | deleteSessionAction;
+
+const sessionsReducer = (state: BookedSession[], action: Action) => {
+  if (action.type === 'ADD_SESSION') {
+    const { payload } = action;
+    const sessionIdx = state.findIndex(
+      (_session) => _session.id === payload.id,
+    );
+
+    if (sessionIdx > -1) {
+      return state;
+    }
+
+    return [...state, payload];
+  }
+
+  if (action.type === 'DELETE_SESSION') {
+    const { id } = action;
+    const sessionIdx = state.findIndex((_session) => _session.id === id);
+
+    if (sessionIdx > -1) {
+      return state.splice(sessionIdx, 1);
+    }
+
+    return state;
+  }
+  return state;
+};
 
 const initialStates: SessionsContextValues = {
   bookedSessions: [],
@@ -25,10 +64,18 @@ const initialStates: SessionsContextValues = {
 const SessionsContext = createContext<SessionsContextValues>(initialStates);
 
 const SessionsProvider = ({ children }: SessionsProviderProps) => {
+  const [bookedSessions, dispatch] = useReducer(
+    sessionsReducer,
+    initialStates.bookedSessions,
+  );
   const value: SessionsContextValues = {
-    bookedSessions: [],
-    addSession: (val) => {},
-    deleteSession: () => {},
+    bookedSessions: bookedSessions,
+    addSession: (val) => {
+      dispatch({ type: 'ADD_SESSION', payload: val });
+    },
+    deleteSession: (id) => {
+      dispatch({ type: 'DELETE_SESSION', id });
+    },
   };
 
   return (
