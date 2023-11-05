3664e91f1b1c75c1794df1a971c338cfa018cb7f
Iris Huang <lyaui@outloook.com>
2023-11-05 23:09:49 +0800

    feat(Header): use sessionsCtx display booked sessions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,7 @@
+import { useRef } from 'react';
 import Button from '@/components/Button';
+import Modal, { ModalRef } from '@/components/Modal';
+import { useSessionsContextVals } from '@/contexts/sessions-context';
 
 function formatNavName(name: string): string {
   let nameArr = name.toLowerCase().split(' ');
@@ -8,25 +11,72 @@ function formatNavName(name: string): string {
 }
 
 function Header() {
+  const modalRef = useRef<ModalRef>(null);
+  const sessionsCtx = useSessionsContextVals();
+
+  const handleButtonClick = () => {
+    modalRef.current?.open();
+  };
+
+  const handleCloseClick = () => {
+    modalRef.current?.close();
+  };
+
+  const handleCancelSession = (id: string) => () => {
+    sessionsCtx.deleteSession(id);
+  };
+
   const navItems = [
     { children: 'our mission', to: '/', isText: true },
     { children: 'browse sessions', to: '/sessions', isText: true },
-    { children: 'upcoming sessions', onClick: () => {} },
+    { children: 'upcoming sessions', onClick: handleButtonClick },
   ];
 
   return (
-    <header id='main-header'>
-      <h1>ReactMentoring</h1>
-      <nav>
-        <ul>
-          {navItems.map((_item) => (
-            <li key={_item.children}>
-              <Button {..._item}>{formatNavName(_item.children)}</Button>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id='main-header'>
+        <h1>ReactMentoring</h1>
+        <nav>
+          <ul>
+            {navItems.map((_item) => (
+              <li key={_item.children}>
+                <Button {..._item}>{formatNavName(_item.children)}</Button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <Modal ref={modalRef}>
+        <h3>Upcoming Session</h3>
+        {sessionsCtx.bookedSessions.length > 0 && (
+          <ul>
+            {sessionsCtx.bookedSessions.map((_session) => (
+              <li key={_session.id}>
+                <div className='upcoming-session'>
+                  <div>
+                    <h3>{_session.title}</h3>
+                    <p>{_session.summary}</p>
+                    <time>{_session.date}</time>
+                  </div>
+                  <div className='actions'>
+                    <Button
+                      onClick={handleCancelSession(_session.id)}
+                      isText={true}
+                    >
+                      cancel
+                    </Button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+
+        <div className='actions'>
+          <Button onClick={handleCloseClick}>Close</Button>
+        </div>
+      </Modal>
+    </>
   );
 }
 
