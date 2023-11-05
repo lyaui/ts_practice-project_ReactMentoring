9a6c2898abf7c20ac58ca3c1b090bbbcfbd02853
Iris Huang <lyaui@outloook.com>
2023-11-05 19:46:07 +0800

    fix(Modal): adjust ModalProps

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,9 +7,8 @@ import {
 import { createPortal } from 'react-dom';
 
 interface ModalProps {
-  title?: string;
   children: ReactNode;
-  actions?: ReactNode;
+
   onClose?: () => void;
 }
 
@@ -18,37 +17,33 @@ export interface ModalRef {
   close: () => void;
 }
 
-const Modal = forwardRef<ModalRef, ModalProps>(
-  ({ title, children, onClose, actions }, ref) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+const Modal = forwardRef<ModalRef, ModalProps>(({ children, onClose }, ref) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    useImperativeHandle(
-      ref,
-      () => ({
-        open() {
-          setIsModalOpen(true);
-        },
-        close() {
-          if (onClose) {
-            onClose();
-          }
-          setIsModalOpen(false);
-        },
-      }),
-      [],
-    );
+  useImperativeHandle(
+    ref,
+    () => ({
+      open() {
+        setIsModalOpen(true);
+      },
+      close() {
+        if (onClose) {
+          onClose();
+        }
+        setIsModalOpen(false);
+      },
+    }),
+    [],
+  );
 
-    const element = window.document.getElementById('modal-root') as HTMLElement;
+  const element = window.document.getElementById('modal-root') as HTMLElement;
 
-    return createPortal(
-      <dialog className='modal' open={isModalOpen}>
-        {title && <h3>{title}</h3>}
-        <p>{children}</p>
-        {actions && <div className='actions'>{actions}</div>}
-      </dialog>,
-      element,
-    );
-  },
-);
+  return createPortal(
+    <dialog className='modal' open={isModalOpen}>
+      {children}
+    </dialog>,
+    element,
+  );
+});
 
 export default Modal;
