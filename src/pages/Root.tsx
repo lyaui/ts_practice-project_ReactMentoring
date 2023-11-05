829a50a914a7efa687a5d71b56864c665d42c3b7
Iris Huang <lyaui@outloook.com>
2023-11-05 21:29:48 +0800

    feat(Root): add SessionsProvider

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '@/components/Header';
+import SessionsProvider from '@/contexts/sessions-context';
 
 export default function Root() {
   return (
-    <>
+    <SessionsProvider>
       <Header />
       <Outlet />
-    </>
+    </SessionsProvider>
   );
 }
