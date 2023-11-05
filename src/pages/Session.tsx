9970ef7f3a002bbd1f1eb2e49b663960084d3949
Iris Huang <lyaui@outloook.com>
2023-11-05 18:13:43 +0800

    feat(Session): add book session modal

--- src/pages/Session.tsx
+++ src/pages/Session.tsx
@@ -1,46 +1,66 @@
+import { useRef } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { SESSIONS } from '../dummy-sessions.ts';
+import Button from '@/components/Button';
+import Modal, { type ModalRef } from '@/components/Modal';
 
 export default function SessionPage() {
+  const modalRef = useRef<ModalRef>(null);
   const params = useParams<{ id: string }>();
 
   const sessionId = params.id;
   const loadedSession = SESSIONS.find((session) => session.id === sessionId);
 
+  const handleOpenClick = () => {
+    modalRef.current?.open();
+  };
+
+  const handleCloseClick = () => {
+    modalRef.current?.close();
+  };
+
   if (!loadedSession) {
     return (
-      <main id="session-page">
+      <main id='session-page'>
         <p>No session found!</p>
       </main>
     );
   }
 
-
   return (
-    <main id="session-page">
-      <article>
-        <header>
-          <img
-            src={loadedSession.image}
-            alt={loadedSession.title}
-          />
-          <div>
-            <h2>{loadedSession.title}</h2>
-            <time dateTime={new Date(loadedSession.date).toISOString()}>
-              {new Date(loadedSession.date).toLocaleDateString('en-US', {
-                day: 'numeric',
-                month: 'short',
-                year: 'numeric',
-              })}
-            </time>
-            <p>
-              {/* Todo: Add button that opens "Book Session" dialog / modal */}
-            </p>
-          </div>
-        </header>
-        <p id="content">{loadedSession.description}</p>
-      </article>
-    </main>
+    <>
+      <main id='session-page'>
+        <article>
+          <header>
+            <img src={loadedSession.image} alt={loadedSession.title} />
+            <div>
+              <h2>{loadedSession.title}</h2>
+              <time dateTime={new Date(loadedSession.date).toISOString()}>
+                {new Date(loadedSession.date).toLocaleDateString('en-US', {
+                  day: 'numeric',
+                  month: 'short',
+                  year: 'numeric',
+                })}
+              </time>
+              <p>
+                <Button onClick={handleOpenClick}>Book Session</Button>
+              </p>
+            </div>
+          </header>
+          <p id='content'>{loadedSession.description}</p>
+        </article>
+      </main>
+      <Modal ref={modalRef}>
+        <h3>Book Session</h3>
+        <p>...</p>
+        <div className='actions'>
+          <Button onClick={handleCloseClick} isText={true}>
+            Cancel
+          </Button>
+          <Button onClick={handleCloseClick}>Book Session</Button>
+        </div>
+      </Modal>
+    </>
   );
 }
